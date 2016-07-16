5134ad57f83b39929478bbc8aa94939217fd4df5
Logan Gorence <loganjohngorence@gmail.com>
2016-07-16 14:09:59 -0700

    Work for CI builds.

--- DSA Mobile/DSA_Mobile.Droid/AndroidApp.cs
+++ DSA Mobile/DSA_Mobile.Droid/AndroidApp.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Android.Content;
 using Android.Hardware;
 using DSAMobile.DeviceSettings;
--- DSA Mobile/DSA_Mobile.Droid/AndroidDSLink.cs
+++ DSA Mobile/DSA_Mobile.Droid/AndroidDSLink.cs
@@ -13,4 +13,3 @@ namespace DSAMobile.Droid
         }
     }
 }
-
